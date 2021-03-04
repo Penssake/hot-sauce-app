45e33d933edf214ac1e650fac5b75508018ce23a
Penssake <penssake@gmail.com>
2021-03-04 00:44:23 -0800

    git add .

--- src/components/HotSauceItem.js
+++ src/components/HotSauceItem.js
@@ -5,7 +5,10 @@ const HotSauceItem = ({ item }) => {
     return (
         <li className="grid-container__item">
             <Icon srcKey="close" classKey="grid-container__close" />
-            <Image srcKey={item.img.src} alt={item.img.alt} classKey="grid-container__product-image" />
+            <div className="grid-container__product-stage">
+                <Image srcKey={item.img.src} alt={item.img.alt} classKey="grid-container__product-image" />
+                <div className="grid-container__product-stage--backlight"></div>
+            </div>
             <h2>{item.title}</h2>
             <p>{item.subtitle}</p>
             <Icon srcKey="arrow" classKey="grid-container__arrow" />
